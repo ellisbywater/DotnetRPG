706c2c99ce95ca0a66ef7ceb8eecd8f09f4490ed
Ellis G <egaraudy@gmail.com>
2020-11-04 15:42:34 -0700

    build base CharacterController and Character Models

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dotnet_Rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> _characters = new List<Character>
+        {
+            new Character {Name = "Aragon", Class = RpgClass.Warden},
+            new Character(),
+            new Character {Name = "Gandalf", Class = RpgClass.Mage}
+        };
+        private static Character knight = new Character();
+        
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(_characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character character)
+        {
+            _characters.Add(character);
+            return Ok(_characters);
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,16 @@
+namespace Dotnet_Rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Stamina { get; set; } = 100;
+        public int Mana { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Willpower { get; set; } = 10;
+        public int Cunning { get; set; } = 10;
+        public int Magic { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
