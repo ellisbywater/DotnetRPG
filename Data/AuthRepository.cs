0447cc6ce98b2a6c1dd76883ce7c258ca9e515ac
Ellis G <egaraudy@gmail.com>
2020-11-05 02:51:09 -0700

    create auth repositories

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,93 @@
+﻿using Dotnet_Rpg.Models;
+using Dotnet_Rpg.Models.Util;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Dotnet_Rpg.Data
+{
+    /// <summary>
+    /// The AuthRepository is where authentication begins
+    /// </summary>
+    public class AuthRepository : IAuthRepository
+    {
+        // Define AuthRepository
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Login to API Service via username and password
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public Task<ServiceResponse<string>> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Register for token authentication via User Model and password
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if(await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists";
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
+        }
+
+        /// <summary>
+        /// Check to see if the User exists
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Create the password hash using the SHA512 algorithm
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+       
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using Dotnet_Rpg.Models;
+using Dotnet_Rpg.Models.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet_Rpg.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string userName, string password);
+        Task<bool> UserExists(string username);
+    }
+}
