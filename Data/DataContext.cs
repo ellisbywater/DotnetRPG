bcda62a28cd354e7af26fb0f557027738dca1fc9
Ellis G <egaraudy@gmail.com>
2020-11-04 22:30:59 -0700

    updat to ef core and datacontext

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Dotnet_Rpg.Models;
+
+namespace Dotnet_Rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
