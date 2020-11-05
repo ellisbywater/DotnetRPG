68a7fe7f4a15a0a3a4008e86cf50ee4232587f9f
Ellis G <egaraudy@gmail.com>
2020-11-04 22:35:22 -0700

    restore branch

---
+++ Models/RpgClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet_Rpg.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Warden = 2,
+        Mage = 3,
+        Assassin = 4
+    }
+}
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dotnet_Rpg.Dtos;
+using Dotnet_Rpg.Dtos.Character;
+using Dotnet_Rpg.Models;
+using Dotnet_Rpg.Models.Util;
+
+namespace Dotnet_Rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters();
+        Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter);
+        Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updateCharacter);
+        Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int id);
+    }
+}
